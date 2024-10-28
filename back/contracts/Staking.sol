f949185f4e03f0ac3718fb3d73db0e0a6ae9e062
Tony-S201 <swierz.tony@gmail.com>
2024-10-28 17:30:28 +0100

    back: create staking function

--- back/contracts/Staking.sol
+++ back/contracts/Staking.sol
@@ -15,8 +15,26 @@ contract Staking {
     mapping(address => StakeInfo) public stakes; // StakeInfo for each address
     uint256 constant REWARD_RATE = 100; // Reward rate
 
+    event Staked(address staker, uint amount); // Event
+
     constructor(address _stakingToken, address _rewardToken) {
         stakingToken = IERC20(_stakingToken); // Define staking token.
         rewardToken = IERC20(_rewardToken); // Define reward token.
     }
+
+    function staking(uint _amount) external {
+        // Check conditions
+        require(_amount > 0, "Amount must be higher than 0");
+        require(stakingToken.balanceOf(msg.sender) > 0, "Not enough token");
+
+        // Transfer tokens to the contract
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update stake informations
+        stakes[msg.sender].amount += _amount;
+        stakes[msg.sender].timestamp = block.timestamp;
+
+        // Emit event
+        emit Staked(msg.sender, _amount);
+    } 
 }
