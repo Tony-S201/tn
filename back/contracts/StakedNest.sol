d73f8fabcb253edb98df6f78d3b58f97ffb8f041
Tony-S201 <swierz.tony@gmail.com>
2024-10-30 10:09:24 +0100

    @slither

--- back/contracts/StakedNest.sol
+++ back/contracts/StakedNest.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Nest is ERC20 {
+contract StakedNest is ERC20 {
     constructor() ERC20("Staked Nest", "stNEST") {
         uint256 initialSupply = 1e9 * (10 ** decimals());
         _mint(msg.sender, initialSupply);
