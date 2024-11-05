55be334943c0e3a1044254c745c9aa8b90075de6
Tony-S201 <swierz.tony@gmail.com>
2024-11-05 14:06:49 +0100

    backend: add and config mongodb and mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!')
-})
+// Connect to DB
+connectDB();
 
+// Start server
 app.listen(port, () => {
     console.log('Listening port ' + port)
+})
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello Express!')
 })
---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const mongoURL = process.env.MONGO_URL;
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(mongoURL);
+        console.log('Database connexion success');
+    } catch(error) {
+        console.error('Database connexion error', error);
+        process.exit(1);
+    }
+};  
+
+module.exports = connectDB;
