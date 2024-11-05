c83641913e857e8e6e43767f4ab2c51c8d709dd6
Tony-S201 <swierz.tony@gmail.com>
2024-11-05 17:55:46 +0100

    front: const

--- frontend/app/constants/tokens.ts
+++ frontend/app/constants/tokens.ts
@@ -1,5 +1,9 @@
-import { type Address } from "viem";
+import { type Abi, type Address } from "viem";
 
 export const stakingAddress: Address | "" = "";
 export const stNestTokenAddress: Address | "" = "";
 export const nestTokenAddress: Address | "" = "";
+
+export const stakingAbi: Abi = [];
+export const stNestTokenAbi: Abi = [];
+export const nestTokenAbi: Abi = [];
